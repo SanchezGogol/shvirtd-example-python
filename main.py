eed5306aa0fc165d52a4c88df7b7b5fbdf48de3f
SanchezGogol <voxoncev2000@mail.ru>
2024-01-14 07:45:46 +0000

    Успешное создание сети и запуск контейнеров

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask import request
 import os
 import mysql.connector
 from datetime import datetime
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 db_host=os.environ.get('DB_HOST')
